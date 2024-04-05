4b3b8f4b1b17512258a3950c22414bf64fc032ae
jmsdaq <jamesdaquioag6@gmail.com>
2024-04-05 08:56:39 +0800

    Cookie Done

--- learn_selenium/cookie.py
+++ learn_selenium/cookie.py
@@ -29,7 +29,19 @@ WebDriverWait(driver, 5).until(
 cookie = driver.find_element(By.ID, cookie_id)
 
 while True:
-	cookie.click()
-	cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
-	cookies_count = int(cookies_count.replace(",", ""))
-	print(cookies_count)
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+    
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
