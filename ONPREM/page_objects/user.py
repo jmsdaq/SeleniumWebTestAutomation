ea0f582ce8208118e1878e20e20396e9bd70247b
jmsdaq <jamesdaquioag6@gmail.com>
2024-04-23 19:53:11 +0800

    update page object

--- ONPREM/page_objects/user.py
+++ ONPREM/page_objects/user.py
@@ -10,10 +10,13 @@ class UserPage(BaseCase):
 
     ADD_BTN = ".btn.btn-success"
     USERNAME = "input.form-control[name='user[name]']"
-    E_CODE = "input.form-control[name='user[employee_code]']"
-    username = "input.form-control[name='user[username]']"
-    password = "input.form-control[name='user[password]']"
-    
+    EMPLOYEE_CODE = "input.form-control[name='user[employee_code]']"
+    USERNAME = "input.form-control[name='user[username]']"
+    PASSWORD = "input.form-control[name='user[password]']"
+    PIN = "input.form-control[name='user[pin]']"
+    OPERATIONAL_ROLE = "input.form-control[name='user[operational_role]']"
+
+
 
     # ONPREM LOCATORS
     ONPREM_MENU = 'a[data-sidebars-target="menu"][href="/nadmin/users"]'
