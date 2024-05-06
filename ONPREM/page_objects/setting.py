e8d8cbcd1cc5d646dd84a92685e491562e7172f8
jmsdaq <jamesdaquioag6@gmail.com>
2024-05-06 16:40:43 +0800

    Settings Locator

--- ONPREM/page_objects/setting.py
+++ ONPREM/page_objects/setting.py
@@ -15,6 +15,27 @@ class SettingPage(BaseCase):
     MACPOS = 'a[data-sidebars-target="menu"][href="/nadmin/macpos/settings"]'
     WAREHOUSE_DISPLAY = 'a[data-sidebars-target="menu"][href="/"]'
 
+    # -------GENERAL LOCATORS--------
+    # Printers
+    PRINTER_NAME = "#printer_config_name"
+    PRINTER_IP = "#printer_config_ip_address"
+    PRINTER_PORT = "#printer_config_port"
+    PRINTER_COPIES = "#printer_config_default_copies"
+    PRINTER_DESC = "#printer_config_description"
+
+    # Checkbox
+    PRINTER_CONF = "#printer_config_enabled"
+    IMAGE_CONF = "#config_request_image_locally"
+    PENDING_CONF = "#config_enable_pending_store"
+    NOTIF_CONF = "#config_new_order_notification"
+    QR_CONF = "#config_qrcode_in_pdf"
+
+    # ------------- MACPOS SETTING LOCATORS -------------
+    ALLOW_LABEL = 'label.form-check-label[for="macpos_setting_incomplete_checkout_allow"]'
+    ALLOW_WITH_APPROVAL_LABEL = 'label.form-check-label[for="macpos_setting_incomplete_checkout_allow_with_approval"]'
+    APP_ID = ".img-responsive"
+
+
     def setting_nav(self):
         self.wait_for_element(self.SIDEBAR_ACTIVE)
         self.assert_element(self.SIDEBAR_ACTIVE) 
