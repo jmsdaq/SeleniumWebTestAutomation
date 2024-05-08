3ce0199b745665bbe6384366cbcfd1e63c229e3a
jmsdaq <jamesdaquioag6@gmail.com>
2024-05-08 10:31:07 +0800

    Test Export

--- ONPREM/page_objects/export.py
+++ ONPREM/page_objects/export.py
@@ -1,15 +1,12 @@
 from seleniumbase import BaseCase
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class ExportPage(BaseCase):
     EXPORT_MENU = "//a[.='articleExports']"
     EXPORT_FROM = 'input#exported_csv_from'
     EXPORT_TO = 'input#exported_csv_to'
     EXPORT_TYPE = 'select#exported_csv_export_type'
+    EXPORT_STATUS = '//*[@id="exported-csvs"]/tbody/tr[1]/td[3]/span'
+    DOWNLOAD = '//*[@id="exported-csvs"]/tbody/tr[2]/td[8]/a'
 
     def export_nav(self):
         self.wait_for_element(self.SIDEBAR_ACTIVE)
--- ONPREM/tests/test_export.py
+++ ONPREM/tests/test_export.py
@@ -1,13 +1,6 @@
 from page_objects.login import LoginPage
 from page_objects.user import UserPage
 from page_objects.export import ExportPage
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import StaleElementReferenceException
-import os
 
 class ExportTest(LoginPage, UserPage, ExportPage):
     def setUp(self):
@@ -22,4 +15,29 @@ class ExportTest(LoginPage, UserPage, ExportPage):
 
     def test_export(self):
         self.export_nav()
+        self.sleep(3)
+
+        # >>>>>>>>>> ADD NEW EXPORT <<<<<<<<<<
+        self.click(self.ADD_BTN)
+        self.sleep(2)
+        self.assert_element(self.MODAL)
+        self.assert_text("New Export", "h1")
+        self.type(self.EXPORT_FROM, "05072024")
+        self.type(self.EXPORT_TO, "06072024")
+
+        select_type = self.EXPORT_TYPE
+        self.click(select_type)
+        # Assuming you want to select the option with the value "order", locate it
+        option_value = "order"
+        option_locator = f"option[value='{option_value}']"
         
+        # Click on the option to select it
+        self.click(option_locator)
+        self.click(self.SUBMIT)
+        self.assert_text("Exporting data. This might take a while...", "h2")
+        self.sleep(5)
+        self.assert_text("processing", self.EXPORT_STATUS)
+        
+    
+        # >>>>>>>>>> DOWNLOAD <<<<<<<<<<
+        self.click(self.DOWNLOAD)
